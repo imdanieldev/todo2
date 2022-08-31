3d77ef910601e6aaae45b6cc0e6c5714dcbed4d8
Daniel Nasr <danialnasronline@gmail.com>
2022-08-31 19:21:58 +0430

    init

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+<main>
+     <h1>ToDo List</h1>
+     <p>Create a list of tasks</p>
+     <div class="create-new">
+          <input 
+          type="text" 
+          v-model="newTask" 
+          placeholder="Add a new task" 
+          @keypress.enter="addTask" />
+          <button @click="addTask">Add</button>
+     </div>
+
+     <div class="tasks">
+          <Task
+          v-for="(task, i) in $store.state.tasks"
+          :key="i"
+          :task="task" />
+     </div>
+</main>
+</template>
+
+<script>
+export default {
+     name: 'IndexPage',
+     data (){
+          return {
+               newTask: ''
+          }
+     },
+     methods: {
+          addTask () {
+               if (this.newTask) {
+                    this.$store.commit('ADD_TASK', this.newTask);
+                    this.newTask = '';
+               }
+          }
+     }
+}
+</script>
